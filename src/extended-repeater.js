35d95f084deef5f3a1d5ecb63e89bec6f0ec5087
Dmitry Ivanov <dimannisik1@gmail.com>
2022-10-12 22:56:33 +0700

    feat: solved the problem extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,25 +1,51 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  if (!options.hasOwnProperty("repeatTimes")) {
+    options.repeatTimes = 1;
+  }
+  if (!options.hasOwnProperty("separator")) {
+    options.separator = "+";
+  }
+  if (!options.hasOwnProperty("addition")) {
+    options.addition = "";
+  }
+  if (!options.hasOwnProperty("additionRepeatTimes")) {
+    options.additionRepeatTimes = 1;
+  }
+  if (!options.hasOwnProperty("additionSeparator")) {
+    options.additionSeparator = "|";
+  }
+
+  const additionsArr = [];
+  for (let i = 0; i < options.additionRepeatTimes; i++) {
+    additionsArr.push(String(options.addition));
+  }
+  const additionsAndSeparator = additionsArr.join(options.additionSeparator);
+  const stringsArr = [];
+  for (let i = 0; i < options.repeatTimes; i++) {
+    stringsArr.push(String(str));
+  }
+  let result = stringsArr.join(`${additionsAndSeparator}${options.separator}`);
+  result += `${additionsAndSeparator}`;
+  return result;
 }
 
 module.exports = {
-  repeater
+  repeater,
 };
