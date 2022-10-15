10006cc1593aea9c310e8c72decae24a734c4199
Dmitry Ivanov <dimannisik1@gmail.com>
2022-10-15 23:44:57 +0700

    feat: solved the problem simple chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,32 +1,58 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement chainMaker object according to task description
- * 
+ *
  */
+
 const chainMaker = {
+  resultArr: [],
+  result: "",
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.resultArr.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    this.resultArr.push(value);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    try {
+      if (
+        typeof position !== "number" ||
+        position - 1 > this.resultArr.length - 1 ||
+        position < 1
+      ) {
+        this.resultArr = [];
+        throw new Error("You can't remove incorrect link!");
+      }
+      this.resultArr.splice(position - 1, 1);
+    } catch (e) {
+      throw e;
+    }
+
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.resultArr = this.resultArr.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    if (this.resultArr.length > 0) {
+      this.result =
+        "( " +
+        this.resultArr
+          .map((val) => {
+            if (val === null) return "null";
+            return val;
+          })
+          .join(" )~~( ") +
+        " )";
+    }
+    this.resultArr.length = 0;
+    return this.result;
+  },
 };
 
 module.exports = {
-  chainMaker
+  chainMaker,
 };
